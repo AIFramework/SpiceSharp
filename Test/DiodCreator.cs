e0d0ec22afe2c74f41caeb1e16b7c1a02efd674e
Zahar A. Ponimash <ponimashz@mail.ru>
2022-03-18 02:52:47 +0300

    Перенос на .Net 4.7.2

---
+++ Test/DiodCreator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using SpiceSharp.Circuits;
+using SpiceSharp.Components;
+
+namespace SpiceSimulation
+{
+    /// <summary>
+    /// Создание модели диода
+    /// </summary>
+    public class DiodCreator
+    {
+        public DiodeModel CreateDiodeModel(string name, string parameters)
+        {
+            var dm = new DiodeModel(name);
+            ApplyParameters(dm, parameters);
+            return dm;
+        }
+
+        protected void ApplyParameters(Entity entity, string definition)
+        {
+            // Get all assignments
+            definition = Regex.Replace(definition, @"\s*\=\s*", "=");
+            var assignments = definition.Split(new[] { ',', ';', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var assignment in assignments)
+            {
+                // Get the name and value
+                var parts = assignment.Split('=');
+                if (parts.Length != 2)
+                    throw new Exception("Invalid assignment");
+                var name = parts[0].ToLower();
+                var value = double.Parse(parts[1], System.Globalization.CultureInfo.InvariantCulture);
+
+                // Set the entity parameter
+                entity.SetParameter(name, value);
+            }
+        }
+    }
+
+    
+}
---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using SpiceSharp;
+using SpiceSharp.Behaviors;
+using SpiceSharp.Components;
+using SpiceSharp.Simulations;
+
+namespace SpiceSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleDC();
+        }
+
+
+        private static void SampleDC()
+        {
+            DiodCreator diodCreator = new DiodCreator();
+
+            var ckt = new Circuit(
+                new VoltageSource("V1", "in", "0", 10),
+                new Resistor("R1", "in", "out", 1.0e3)
+                );
+
+            // Добавление диода
+            ckt.Add(new Diode("D1", "out", "0", "1N914"), 
+                diodCreator.CreateDiodeModel("1N914", "Is=2.52e-9 Rs=0.568 N=1.752 Cjo=4e-12 M=0.4 tt=20e-9")); // Параметры из LTSpice
+
+            var dc = new DC("dc", "V1", 0.0, 150.0, 1);
+
+            dc.ExportSimulationData += (sender, exportDataEventArgs) =>
+            {
+                Console.WriteLine(exportDataEventArgs.GetVoltage("out")); ;
+            };
+
+
+            dc.Run(ckt);
+        }
+
+
+    }
+
+    
+}
