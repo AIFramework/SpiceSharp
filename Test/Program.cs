ef098ab77e0e238c7ae64b6797e5c8a600043d5d
Zahar A. Ponimash <ponimashz@mail.ru>
2022-03-18 13:16:20 +0300

    Анализ по переменному току

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,33 +13,76 @@ namespace SpiceSimulation
     {
         static void Main(string[] args)
         {
-            SampleDC();
+            // Схема
+            var ckt = new Circuit(
+               new VoltageSource("V1", "in", "0", 0),
+               new Resistor("R1", "in", "out", 1.0e3),
+               new Inductor("L1", "out", "0", 1e-3),
+               new Capacitor("C1", "out", "0", 1.0e-6),
+               new Resistor("R2", "out", "0", 1e+5)
+               );
+
+            var ac = new AC("ac", new LinearSweep(1e+3, 6e+3, 1000)); // Анализатор
+
+            // Анализ по переменному току
+            ACAnalyzer(ckt, ac);
+
         }
 
-
-        private static void SampleDC()
+        /// <summary>
+        /// Анализ по переменному току
+        /// </summary>
+        private static void ACAnalyzer(Circuit ckt, AC ac, string vSource = "V1")
         {
-            DiodCreator diodCreator = new DiodCreator();
 
-            var ckt = new Circuit(
-                new VoltageSource("V1", "in", "0", 10),
-                new Resistor("R1", "in", "out", 1.0e3)
-                );
-
-            // Добавление диода
-            ckt.Add(new Diode("D1", "out", "0", "1N914"), 
-                diodCreator.CreateDiodeModel("1N914", "Is=2.52e-9 Rs=0.568 N=1.752 Cjo=4e-12 M=0.4 tt=20e-9")); // Параметры из LTSpice
-
-            var dc = new DC("dc", "V1", 0.0, 150.0, 1);
-
-            dc.ExportSimulationData += (sender, exportDataEventArgs) =>
+            foreach (var item in ckt)
             {
-                Console.WriteLine(exportDataEventArgs.GetVoltage("out")); ;
+                if (item.Name == vSource)
+                {
+                    item.SetParameter("acmag", 1.0);
+                }
+            }
+
+            var exportVoltage = new ComplexVoltageExport(ac, "out");
+            var exportVoltageIn = new ComplexVoltageExport(ac, "in");
+
+            ac.ExportSimulationData += (sender, exportDataEventArgs) =>
+            {
+                var output = exportVoltage.Value.Magnitude;
+                var input = exportVoltageIn.Value.Magnitude;
+                var K = output / input;
+                Console.WriteLine($"Коэф. передачи: {K},\t\t Частота: {exportDataEventArgs.Frequency}");
             };
 
 
-            dc.Run(ckt);
+            ac.Run(ckt);
         }
+
+
+        //private static void DCAnalyzer()
+        //{
+        //    DiodCreator diodCreator = new DiodCreator();
+
+        //    var ckt = new Circuit(
+        //        new VoltageSource("V1", "in", "0", 10),
+        //        new Resistor("R1", "in", "out", 1.0e3)
+        //        );
+
+        //    // Добавление диода
+        //    ckt.Add(new Diode("D1", "out", "0", "1N914"),
+        //        diodCreator.CreateDiodeModel("1N914", "Is=2.52e-9 Rs=0.568 N=1.752 Cjo=4e-12 M=0.4 tt=20e-9")); // Параметры из LTSpice
+
+        //    var dc = new DC("dc", "V1", 0.0, 150.0, 1);
+
+        //    dc.ExportSimulationData += (sender, exportDataEventArgs) =>
+        //    {
+        //        Console.WriteLine(exportDataEventArgs.GetVoltage("out")); ;
+        //    };
+
+
+        //    dc.Run(ckt);
+        //}
+
 
 
     }
