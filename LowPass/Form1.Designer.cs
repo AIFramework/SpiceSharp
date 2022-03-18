7dc18c093987dbb90f900e7c694f19d05a15aeca
Zahar A. Ponimash <ponimashz@mail.ru>
2022-03-19 00:42:32 +0300

    Симуляция фильтра НЧ

---
+++ LowPass/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LowPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chartVisual1 = new AI.Charts.Control.ChartVisual();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.freqCut = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.order = new System.Windows.Forms.TextBox();
+            this.C3l = new System.Windows.Forms.Label();
+            this.C2l = new System.Windows.Forms.Label();
+            this.nC = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartVisual1
+            // 
+            this.chartVisual1.AutoScroll = true;
+            this.chartVisual1.BackColor = System.Drawing.Color.White;
+            this.chartVisual1.ChartName = "АЧХ";
+            this.chartVisual1.ForeColor = System.Drawing.Color.Black;
+            this.chartVisual1.IsContextMenu = true;
+            this.chartVisual1.IsLogScale = false;
+            this.chartVisual1.IsMoove = true;
+            this.chartVisual1.IsScale = true;
+            this.chartVisual1.IsShowXY = true;
+            this.chartVisual1.LabelX = "Частота";
+            this.chartVisual1.LabelY = "Коэф. передачи";
+            this.chartVisual1.Location = new System.Drawing.Point(341, 1);
+            this.chartVisual1.Name = "chartVisual1";
+            this.chartVisual1.Size = new System.Drawing.Size(724, 339);
+            this.chartVisual1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Частота среза:";
+            // 
+            // freqCut
+            // 
+            this.freqCut.Location = new System.Drawing.Point(90, 169);
+            this.freqCut.Name = "freqCut";
+            this.freqCut.Size = new System.Drawing.Size(110, 20);
+            this.freqCut.TabIndex = 3;
+            this.freqCut.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Анализ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Порядок:";
+            // 
+            // order
+            // 
+            this.order.Location = new System.Drawing.Point(90, 196);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(110, 20);
+            this.order.TabIndex = 6;
+            this.order.Text = "6";
+            // 
+            // C3l
+            // 
+            this.C3l.AutoSize = true;
+            this.C3l.Location = new System.Drawing.Point(2, 244);
+            this.C3l.Name = "C3l";
+            this.C3l.Size = new System.Drawing.Size(20, 13);
+            this.C3l.TabIndex = 7;
+            this.C3l.Text = "С3";
+            // 
+            // C2l
+            // 
+            this.C2l.AutoSize = true;
+            this.C2l.Location = new System.Drawing.Point(2, 268);
+            this.C2l.Name = "C2l";
+            this.C2l.Size = new System.Drawing.Size(20, 13);
+            this.C2l.TabIndex = 8;
+            this.C2l.Text = "С2";
+            // 
+            // nC
+            // 
+            this.nC.AutoSize = true;
+            this.nC.Location = new System.Drawing.Point(2, 301);
+            this.nC.Name = "nC";
+            this.nC.Size = new System.Drawing.Size(90, 13);
+            this.nC.TabIndex = 9;
+            this.nC.Text = "Число каскадов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 339);
+            this.Controls.Add(this.nC);
+            this.Controls.Add(this.C2l);
+            this.Controls.Add(this.C3l);
+            this.Controls.Add(this.order);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.freqCut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chartVisual1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Фильтр Баттервордта";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AI.Charts.Control.ChartVisual chartVisual1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox freqCut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox order;
+        private System.Windows.Forms.Label C3l;
+        private System.Windows.Forms.Label C2l;
+        private System.Windows.Forms.Label nC;
+    }
+}
+
---
+++ LowPass/Form1.cs
@@ -0,0 +1,158 @@
+﻿using AI.DataStructs.Algebraic;
+using SpiceSharp;
+using SpiceSharp.Circuits;
+using SpiceSharp.Components;
+using SpiceSharp.Simulations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LowPass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            circuit = CreateCrk(n_blocks: 3, c1: 2.5e-6, c2: 1.3e-6); // Фильтр 6 порядка
+        }
+
+        Circuit circuit;
+        int max_block;
+
+
+        double CalcDenom(double fCut, double r) 
+        {
+            return Math.PI * 2 * fCut * r;
+        }
+
+        double CalcC1(double fCut, double r)
+        {
+            return 1.114 / CalcDenom(fCut, r);
+        }
+
+        double CalcC2(double fCut, double r)
+        {
+            return 0.707 / CalcDenom(fCut, r);
+        }
+
+        // Параметры одного блока ФНЧ
+        Entity[] FilterBlockFromParam(double c1 = 1.44e-6, double c2 = 1.09e-6, double inpC = 1e-6, double r=1e+3, int n_block =1)
+        {
+            //Транзистор
+            var bPolar = new Mosfet1("M1_"+n_block) { Model = "bPolar" };
+            bPolar.Connect("E", "m3_" + n_block, "out_" + n_block, "out_" + n_block);
+
+            Entity[] block =
+            {
+               new Capacitor("C_"+n_block, "out_"+(n_block-1), "m_"+n_block, inpC),
+               new Resistor("R1_"+n_block, "E", "m_"+n_block, 1.0e+4),
+               new Resistor("R2_"+n_block, "m_"+n_block, "0", 1.0e+4),
+
+               new Resistor("R3_"+n_block, "m_"+n_block, "m2_"+n_block, r),
+               new Resistor("R4_"+n_block, "m2_"+n_block, "m3_"+n_block, r),
+
+               new Capacitor("C1_"+n_block, "m2_"+n_block, "out_"+n_block, c1),
+               new Capacitor("C2_"+n_block, "m3_"+n_block, "0", c2),
+               new Resistor("R5_"+n_block, "0", "out_"+n_block, 5.6e+3),
+               bPolar
+            };
+
+            return block;
+        }
+
+        // Много блоков
+        Circuit CreateCrk(double c1 = 1.44e-6, double c2 = 1.09e-6, double inpC = 1000e-6, double r = 1e+3, int n_blocks = 1) 
+        {
+            // Создание модели транзистора
+            var bPolar_model = new Mosfet1Model("bPolar");
+            bPolar_model.SetParameter("kp", 150.0e-3);
+
+            max_block = n_blocks;
+            Circuit crk = new Circuit();
+
+            for (int i = 0; i < n_blocks; i++)
+            {
+                Entity[] entities = FilterBlockFromParam(c1, c2, inpC, r, i+1);
+                for (int j = 0; j < entities.Length; j++)
+                {
+                    crk.Add(entities[j]);
+                }
+            }
+
+            crk.Add(new VoltageSource("V1", "E", "0", 5)); // Питание
+            crk.Add(new VoltageSource("V2", "out_0", "0", 0)); // Сигнал
+            crk.Add(bPolar_model); // Транзистор
+
+            return crk;
+        }
+
+
+
+        /// <summary>
+        /// Анализ по переменному току
+        /// </summary>
+        private void ACAnalyzer(Circuit ckt, AC ac, string vSource = "V2")
+        {
+
+            foreach (var item in ckt)
+            {
+                if (item.Name == vSource)
+                {
+                    item.SetParameter("acmag", 1.0);
+                }
+            }
+
+            var exportVoltage = new ComplexVoltageExport(ac, "out_"+max_block);
+            var exportVoltageIn = new ComplexVoltageExport(ac, "out_0");
+            Vector k = new Vector(0);
+            Vector f = new Vector(0);
+
+            ac.ExportSimulationData += (sender, exportDataEventArgs) =>
+            {
+                var output = exportVoltage.Value.Magnitude;
+                var input = exportVoltageIn.Value.Magnitude;
+                var K = output / input;
+                k.Add(K);
+                f.Add(exportDataEventArgs.Frequency);
+                
+                if(f.Count == 900) chartVisual1.PlotBlack(f, k);
+            };
+
+
+            ac.Run(ckt);
+        }
+
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(order.Text) / 2;
+            double fCut = Convert.ToDouble(freqCut.Text, AI.AISettings.GetProvider());
+            double c1 = CalcC1(fCut, 1e3);
+            double c2 = CalcC2(fCut, 1e3);
+
+            C3l.Text = "C3 = "+ Math.Round(c1 *1e6, 3) +" мкФ";
+            C2l.Text = "C2 = "+ Math.Round(c2 * 1e6, 3) + " мкФ";
+            nC.Text = "Число каскадов: " + n;
+
+
+            circuit = CreateCrk(n_blocks: n, c1:c1, c2: c2); // Фильтр заданного порядка
+
+            var ac = new AC("ac", new LinearSweep(0, 2* fCut, 1000)); // Анализатор
+            ACAnalyzer(circuit, ac);
+        }
+    }
+}
